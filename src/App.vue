191fcbec3c3f03ef91a74bb9b1c49d9577f73ee8
x萱 <2916880113@qq.com>
2024-05-23 19:53:38 +0800

    “init”

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+<router-view></router-view>
+</template>
+<script setup lang='ts'>
+
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<style lang='scss' scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+11111
+
+</template>
+<script setup lang='ts'>
+
+
+</script>
+<style lang='scss' scoped>
+
+</style>
